3ca783a30bfcf42b84c2f94bdf99046c3e2dd6da
thevernabean <thevernabean@gmail.com>
2012-05-20 17:10:37 -0700

    Change to VernaBranch of FolderAutoBackupUtility Added new class VernaZippingPractice. Started implementing methods for use in producing Zip Archives.

--- src/folderautobackuputility/VernaZippingPractice.java
+++ src/folderautobackuputility/VernaZippingPractice.java
@@ -5,10 +5,63 @@
 
 package folderautobackuputility;
 
+import java.io.File;
+import java.util.LinkedList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
 /**
  *
  * @author Sweord
  */
 public class VernaZippingPractice {
+
+    private ZipOutputStream archive;
+    LinkedList<ZipEntry> entries;
+    File zipArchiveLocation;
+
+    /**
+     * Adds a ZipEntry to the linked list of entries.
+     * @param e The zip entry to add
+     * @return Boolean indicating whether or not the addition was successful
+     */
+    public boolean addEntry(ZipEntry e) {
+        return entries.add(e);
+}
+
+    /**
+     *
+     * @param e
+     * @return
+     */
+    public boolean addEntry(File e) {
+        ZipEntry entry = new ZipEntry(e.getAbsolutePath());
+        
+        return entries.add(entry);
+    }
+
+    /**
+     *
+     */
+    public void writeFile(){
+
+    }
+
+    /**
+     *
+     * @param zipArchiveToWriteTo
+     */
+    public VernaZippingPractice(File zipArchiveToWriteTo) {
+        try {
+            zipArchiveLocation = zipArchiveToWriteTo;
+
+
+
+        } catch (Exception ex) {
+            Logger.getLogger(VernaZippingPractice.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 
 }
